3be0a83824ab908ecc99e9c53873b8c67590ae0c
Syncxv <aruldevarackal1@gmail.com>
2022-03-12 19:09:53 +0530

    models

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final String id;
+  final String username;
+  final String displayName;
+  final String avatar;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.displayName,
+    required this.avatar,
+  });
+}
