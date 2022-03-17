02968793c8d8a5fd94ade00b291c5a28e0360913
Syncxv <aruldevarackal1@gmail.com>
2022-03-17 23:46:18 +0530

    graphql thingy

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:sma_flutter/screens/nav_screen.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await initHiveForFlutter();
+  final HttpLink httpLink = HttpLink(
+    'http://192.168.29.23:8000/graphql',
+  );
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      // The default store is the InMemoryStore, which does NOT persist to disk
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+  runApp(MyApp(client: client));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final ValueNotifier<GraphQLClient> client;
+  const MyApp({Key? key, required this.client}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'SMA THINGY ong',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: NavScreen(),
       ),
-      home: NavScreen(),
     );
   }
 }
